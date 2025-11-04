08e04d6322fd844cd8e01e4b9b7961379503500b
Sandwichzzy <62085707+Sandwichzzy@users.noreply.github.com>
2025-11-04 03:48:58 +0000

    modules

--- foundry-twt-treasure/src/TreasureManager.sol
+++ foundry-twt-treasure/src/TreasureManager.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
@@ -14,7 +14,7 @@ contract TreasureManager is
     Initializable,
     OwnableUpgradeable,
     AccessControlUpgradeable,
-    ReentrancyGuardUpgradeable,
+    ReentrancyGuard,
     ITreasureManager
 {
     using SafeERC20 for IERC20;
